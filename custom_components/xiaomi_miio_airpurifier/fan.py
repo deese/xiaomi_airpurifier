b94c134ee58d0dd94d297f1a238f8410301881f2
deese <deese2k@gmail.com>
2026-02-19 21:57:40 +0100

    Set unique_id format for device initialization

--- custom_components/xiaomi_miio_airpurifier/fan.py
+++ custom_components/xiaomi_miio_airpurifier/fan.py
@@ -1107,7 +1107,7 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
     retries = config[CONF_RETRIES]
 
     _LOGGER.info("Initializing with host %s (token %s...)", host, token[:5])
-    unique_id = None
+    unique_id = f"{host}-{token[:5]}"
 
     if model is None:
         try:
